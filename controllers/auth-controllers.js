d606ae9d29f3a1c67dabba4f569feee9ccdf40ae
halljosh <joshuaandrewhall@gmail.com>
2020-09-11 16:43:34 -0400

    fixed some stuff

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,5 +1,6 @@
 const User = require('../models/user-model');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 exports.getLogin = (req, res, next) => { 
     let errorMessage = req.flash('loginerror');
@@ -89,3 +90,55 @@ exports.postSignup = (req, res, next) => {
             console.log(err);
         });
 };
+
+exports.getReset = (req, res, next) => {
+    let errorMessage = req.flash('reseterror');
+    if (errorMessage.length > 0) {
+        errorMessage = errorMessage[0];
+    } else {
+        errorMessage = null;
+    }
+    res.render('auth-views/reset', {
+        path: '/reset',
+        docTitle: 'reset password',
+        errorMessage: errorMessage,
+    })
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User
+            .findOne({username: req.body.username})
+            .then(user => {
+                if (!user) {
+                    req.flash('reseterror', 'no account found! check your spelling?')
+                    return res.redirect('reset');
+                }
+                user.resetToken = token;
+                return user.save();
+            })
+            .then(result => {
+                res.redirect('/reset/' + token);
+            })
+            .catch(err => console.log(err));
+    });
+};
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User
+        .findOne({resetToken: token})
+        .then(user => {
+            res.render('auth-views/new-password', {
+                path: '/new-password',
+                docTitle: 'reset password',
+                id: user._id.toString()
+            });
+        })
+        .catch(err => console.log(err));
+}; 
